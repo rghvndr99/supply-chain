b8ac9502bad1cd8c8d6ae5da6b2b957f277a8d40
rdixi4 <rdixit5@sapient.com>
2019-12-23 16:38:57 +0530

    removing class based component

--- src/components/Tile.js
+++ src/components/Tile.js
@@ -43,51 +43,6 @@ const Tile=(props)=>{
 		</div>
 		)
 }
-/*class Tile extends React.Component {
-	constructor (props) {
-		super(props);
-		this.editProductDetails = this.editProductDetails.bind(this);		
-	}
-	editProductDetails = () =>{
-		const {editProduct} = this.props;
-		editProduct();
-	}
 
-	render() {
-		const { loginAssupplier,removeFromlist,product,editProduct} = this.props;
-		const { Name, Description, SupplierName, Quantity=1,ProductId} = product;
-		const imgAry=[image1,image2,image3,image4,image5,image6,image7,image8];
-		const imgName=imgAry[Math.floor(Math.random() * imgAry.length-1) + 1]; 
-		
-		return (
-			<div className="card">
-				<div className ="product-info">
-  				<img src={imgName} className="product-image" />
-  				<div className="container">
-   					 <p className="name"><b>{Name}</b></p>
-    				 <p className="product">{Description}</p>
-    				 <div className="more-info">
-    				 	<span className="supplied-by"> 
-    				 		Supplier: 
-    				 		<span className="legend">
-    				 			{SupplierName}
-    				 		</span>
-    				 	</span> 
-						 <span className="quantity"> quantity: 
-    				 		<span className="legend">
-								{Quantity}
-    				 		</span>    				 		
-						 </span>  
-    				 </div>
-  				</div>
-				 </div>
-					<div className = "user-action">
-							<button className ="btn btn-remove" onClick={()=>removeFromlist(ProductId)} >Remove</button>
-							<button className="btn btn-edit" onClick={()=>editProduct(product)}>Edit</button>
-					</div>
-			</div>
-			)
-	}
-}*/
 
 export default Tile;
