7d66e8fc93ae6b11748ddc651c2fab201b5fc984
rdixi4 <rdixit5@sapient.com>
2020-01-02 12:55:44 +0530

    database security

--- server/index.js
+++ server/index.js
@@ -16,10 +16,25 @@ import express from 'express';
 import mongoDB from 'mongodb';
 import bodyParser from 'body-parser';
 import axios from 'axios';
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
 import { async } from 'q';
 
 const mongoClient = mongoDB.MongoClient;
 const app = express();
+
+app.use(helmet());
+const limit = rateLimit({
+    max: 100,
+    windowMs: 60 * 60 * 1000,  
+    message: 'Too many requests'
+});
+app.use('/routeName', limit); 
+app.use(express.json({ limit: '10kb' })); 
+app.use(mongoSanitize());
+app.use(xss());
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -30,6 +45,8 @@ app.use((req, res, next) => {
   res.append('Access-Control-Allow-Credentials', 'true');
   next();
 });
+
+
 
 mongoClient.connect(config.mongodbUri, {
 	useNewUrlParser: true,
